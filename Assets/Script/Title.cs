9f331e9046e3a19e31a51b60b5a2b1dd3cd3aba5
Hyangyoun <thakd7092@naver.com>
2020-11-27 21:46:34 +0900

    Title 추가

---
+++ Assets/Script/Title.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public GameObject text;
+    private Animator anim;
+    bool touch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(TouchStart());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (touch&&Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Goto");
+            StartCoroutine(GotoPrologue());
+        }
+    }
+
+    IEnumerator TouchStart()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("맞나?");
+        text.SetActive(true);
+        touch = true;
+    }
+    IEnumerator GotoPrologue()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("Prologue");
+    }
+}
